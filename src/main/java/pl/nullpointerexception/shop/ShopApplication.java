5613070a3d705c55c79c398466199ac746b31404
Adrian Zawadzki <zawadzkii@tlen.pl>
2022-12-15 10:26:51 +0100

    liquibase and mysql configuration

--- src/main/java/pl/nullpointerexception/shop/ShopApplication.java
+++ src/main/java/pl/nullpointerexception/shop/ShopApplication.java
@@ -2,27 +2,23 @@ package pl.nullpointerexception.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class })
 public class ShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer cors() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:4200");
-			}
-		};
-	}
+//	@Bean
+//	public WebMvcConfigurer cors() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**")
+//						.allowedOrigins("http://localhost:4200");
+//			}
+//		};
+//	}
 
 }
